e29f90ecd546458080e8bed3850e246b50c4d496
Verbiush <jupacan07@gmail.com>
2026-03-20 15:57:48 -0500

    Auto-update agent gui fields to the new AWS IP address if old IP is found in config

--- src/local_agent/main.py
+++ src/local_agent/main.py
@@ -2341,7 +2341,7 @@ class AgentGUI:
         default_task_url = "http://3.15.237.186:8000/tasks/poll"
         # Heuristic: if loaded config has "localhost" or "8501" or "/api/", replace with correct default
         loaded_task_url = self.config.get("task_url", default_task_url)
-        if "localhost" in loaded_task_url or "8501" in loaded_task_url or "/api/" in loaded_task_url:
+        if "localhost" in loaded_task_url or "8501" in loaded_task_url or "/api/" in loaded_task_url or "3.142.164.128" in loaded_task_url:
              loaded_task_url = default_task_url
              
         self.url_task_var = tk.StringVar(value=loaded_task_url)
@@ -2350,7 +2350,7 @@ class AgentGUI:
         ttk.Label(frame, text="URL Resultados:").grid(row=3, column=0, sticky=tk.W)
         default_res_url = "http://3.15.237.186:8000/tasks"
         loaded_res_url = self.config.get("result_url", default_res_url)
-        if "localhost" in loaded_res_url or "8501" in loaded_res_url or "/api/" in loaded_res_url:
+        if "localhost" in loaded_res_url or "8501" in loaded_res_url or "/api/" in loaded_res_url or "3.142.164.128" in loaded_res_url:
              loaded_res_url = default_res_url
              
         self.url_res_var = tk.StringVar(value=loaded_res_url)
